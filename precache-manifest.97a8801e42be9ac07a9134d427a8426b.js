a51e8f6d49c2e79230b79f832d5447b28bbdd7aa
alex-glomo <alexandr@glomo.se>
2020-02-18 15:15:00 +0100

    Updates

---
+++ precache-manifest.97a8801e42be9ac07a9134d427a8426b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d199b856d06356acdcbb2aade3174884",
+    "url": "/slot-machine/index.html"
+  },
+  {
+    "revision": "ee60010e3dae76b1ebb1",
+    "url": "/slot-machine/static/css/main.a3b6f5c7.chunk.css"
+  },
+  {
+    "revision": "096338bb50f843a3cdb2",
+    "url": "/slot-machine/static/js/2.0eb1205c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/slot-machine/static/js/2.0eb1205c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee60010e3dae76b1ebb1",
+    "url": "/slot-machine/static/js/main.c900a75f.chunk.js"
+  },
+  {
+    "revision": "644a86891a34759c5ca7",
+    "url": "/slot-machine/static/js/runtime-main.0bd53c6c.js"
+  }
+]);
